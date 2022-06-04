9e79349732571423c24575bb19c1d8baaa2c4832
Zisp0 <92941421+Zisp0@users.noreply.github.com>
2022-06-03 21:47:05 -0500

    Update settings.py

--- maquinaTuring/maquinaTuring/settings.py
+++ maquinaTuring/maquinaTuring/settings.py
@@ -127,6 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Update database configuration with $DATABASE_URL.
 import dj_database_url  
+import os
 db_from_env = dj_database_url.config(conn_max_age=500)  
 DATABASES['default'].update(db_from_env)
 
